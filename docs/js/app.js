55a56b1fd68067dbe9300dda65264ce2669d4f94
Muhan Li <limuhan@msn.com>
2023-12-08 23:57:10 +0800

    new homepage design

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,217 +1,143 @@
-var app = new Vue({
-  el: "#head",
-  data: {
-    title: "Muhan Li \u2005\u2502\u2005 About me",
-  },
-});
+import {data, getTranslation} from "./data.js";
 
-var content = new Vue({
-  el: "#content",
+const update = "December 8, 2023";
+
+const app = new Vue({
+  el: "#app",
   data: {
     loading: true,
-    en: true,
-    zh: false,
-    info: {
-      name: "Li,Muhan".replace(/,M/, "m"),
-      mail: "\u0040msn\u002ecom",
-      city: "Singapore",
-      tz: "UTC+8",
-      update: "August 18, 2023",
-    },
-    edu: {
-      hitsz: {
-        start: {
-          yr: 2017,
-          mo: 8,
-        },
-        leave: {
-          yr: 2021,
-          mo: 6,
-        },
-        name: {
-          en: "Harbin Institute of Technology",
-          zh: "哈尔滨工业大学",
-        },
-        url: {
-          en: "http://en.hitsz.edu.cn/",
-          zh: "https://www.hitsz.edu.cn/index.html",
-        },
-        grade: {
-          en: "Grade: 88%",
-          zh: "GPA: 88\u2009/\u2009100",
-        },
-        major: {
-          en: "Bachelor of Engineering · Communication Engineering",
-          zh: "工学学士 · 通信工程专业",
-        },
-        school: {
-          en: "School of Electronics and Information Engineering",
-          zh: "电子与信息工程学院",
-        },
-        city: {
-          en: "Shenzhen · China",
-          zh: "中国 · 深圳",
-        },
-      },
-      ntu: {
-        start: {
-          yr: 2023,
-          mo: 8,
-        },
-        leave: {
-          yr: 2024,
-          mo: 7,
-        },
-        name: {
-          en: "Nanyang Technological University",
-          zh: "南洋理工大学",
-        },
-        url: {
-          en: "https://www.ntu.edu.sg/index",
-          zh: "https://www.ntu.edu.sg/main/cn",
-        },
-        grade: {
-          en: "TBA",
-          zh: "TBA",
-        },
-        major: {
-          en: "Master of Science · Artificial Intelligence",
-          zh: "理学硕士 · 人工智能专业",
-        },
-        school: {
-          en: "School of Computer Science and Engineering",
-          zh: "计算机科学与工程学院",
-        },
-        city: {
-          en: "Singapore",
-          zh: "新加坡",
-        },
-      },
-    },
-    exp: {
-      baidu: {
-        start: {
-          yr: 2021,
-          mo: 8,
-        },
-        leave: {
-          yr: 2023,
-          mo: 8,
-        },
-        lasting: {
-          en: "2 years",
-          zh: "2 年",
-        },
-        name: {
-          en: "Baidu",
-          zh: "百度",
-        },
-        url: {
-          en: "https://ir.baidu.com/",
-          zh: "https://home.baidu.com/",
-        },
-        position: {
-          en: "Software Engineer · Search Tech Platform R&D Dept",
-          zh: "软件工程师 · 搜索技术平台研发部 · MEG",
-        },
-        skills: {
-          en: "Baidu App Backend Developer · Golang",
-          zh: "手机百度后端研发 · 主力语言 Go",
-        },
-        city: {
-          en: "Beijing · China",
-          zh: "中国 · 北京",
-        },
-      },
-    },
-    current: {
-      start: {
-        yr: 2023,
-        mo: 8,
-      },
-      leave: {
-        yr: 2024,
-        mo: 7,
-      },
-      present: {
-        en: "Present",
-        zh: "至今",
-      },
-      lasting: {
-        en: "Recently",
-        zh: "最近",
-      },
-    },
+    lang: "en-US",
+    name: "Li, Muhan".replace(/, M/, "m"),
+    mail: "\u0040msn\u002Ecom",
+    city: "Singapore",
+    time: "09:41",
+    utcD: "UTC+8",
+    tz: "Asia/Singapore",
+    update: update,
+    year: new Date().getFullYear(),
+    skills: data.skills,
+    scroll: data.skills_scroll_frames,
+    projects: [],
+    contents: [],
   },
 
   created() {
     let urlParams = new URLSearchParams(window.location.search);
-    let ok = urlParams.has("lang");
-    let zh = ok && urlParams.get("lang").startsWith("zh");
-    this.en = !zh;
-    this.zh = zh;
-
-    const d = new Date();
-    let yr = d.getFullYear();
-    let mo = d.getMonth() + 1;
-    let da = d.getDate();
-
-    this.current.leave.yr = yr;
-    this.current.leave.mo = mo;
-
-    let start = this.current.start;
-    let m = 12 * (yr - start.yr) + (mo - start.mo);
-    if (da > 15) {
-      // m += 1;
+    if (urlParams.has("lang")) {
+      this.lang = urlParams.get("lang");
     }
 
-    let years = parseInt(m / 12);
-    let months = m % 12;
+    fillContents(this);
 
-    let english = "";
-    let chinese = "";
-
-    if (years > 0) {
-      english += years + " year";
-      chinese += years + " 年";
-      if (years > 1) {
-        english += "s";
-      }
-      if (months > 0) {
-        english += " ";
-        chinese += " ";
-      }
-    }
-
-    if (months > 0) {
-      english += months + " month";
-      chinese += months + " 个月";
-      if (months > 1) {
-        english += "s";
-      }
-    }
-
-    if (years > 0 || months > 0) {
-      this.current.lasting.en = english;
-      this.current.lasting.zh = chinese;
-    }
+    this.utcD = getUtcOffset(this.tz);
+    this.time = getLocalTime(this.tz);
   },
 
   mounted() {
-    that = this;
-    setTimeout(function () {
-      that.loading = false;
+    this.scroll.forEach((_, index) => {
+      this.initiateScrollForRow(index);
+    });
+
+    setInterval(() => {
+      this.time = getLocalTime(this.tz);
+    }, 1000);
+
+    setTimeout(() => {
+      this.loading = false;
     }, 100);
   },
 
   methods: {
-    lang_en(event) {
-      this.en = true;
-      this.zh = false;
+    switch_lang(event) {
+      if (this.lang === "en-US") {
+        this.lang = "zh-CN";
+      } else {
+        this.lang = "en-US";
+      }
+      console.log(this.lang);
+      fillContents(this);
     },
-    lang_zh(event) {
-      this.en = false;
-      this.zh = true;
+
+    initiateScrollForRow(index) {
+      let delta = 0;
+      const step = () => {
+        if (this.scroll[index] > 0) {
+          let display = this.$refs.scrollDisplay[index];
+          let displayWidth = display.getBoundingClientRect().width;
+
+          let content = this.$refs.scrollContent[index];
+          let contentWidth = content.getBoundingClientRect().width / 2;
+
+          delta = (delta + displayWidth / this.scroll[index]) % contentWidth;
+
+          let x = index % 2 ? delta : contentWidth - delta;
+          display.style.transform = `translateX(${-x}px)`;
+        }
+        requestAnimationFrame(step);
+      };
+      requestAnimationFrame(step);
     },
+
+    pauseScroll(index) {
+      this.scroll[index] = -Math.abs(this.scroll[index]);
+    },
+
+    resumeScroll(index) {
+      this.scroll[index] = Math.abs(this.scroll[index]);
+    }
   },
 });
+
+function fillContents(that) {
+  const translated = getTranslation(that.lang);
+  that.projects = translated.projects;
+  that.contents = [{
+    cn: "工作经历",
+    en: "Experience",
+    data: translated.experiences,
+  }, {
+    cn: "教育背景",
+    en: "Education",
+    data: translated.educations,
+  }];
+}
+
+function getLocalTime(timeZone) {
+  const now = new Date();
+  const formattedTime = now.toLocaleString('en-US', {
+    timeZone: timeZone,
+    hour12: false,
+    hourCycle: 'h23',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  let [hour, minute, second] = formattedTime.split(':');
+  if (hour === '24') {
+    hour = '00';
+  }
+
+  return `${hour}:${minute}`;
+}
+
+function getUtcOffset(timeZone) {
+  const now = new Date();
+  const utcDate = new Date(now.toLocaleString('en-US', {timeZone: 'UTC'}));
+  const tzDate = new Date(now.toLocaleString('en-US', {timeZone: timeZone}));
+
+  let offset = (tzDate - utcDate) / (3600 * 1000);
+
+  // Handle daylight saving time
+  const isDaylightSaving = tzDate.getTimezoneOffset() < utcDate.getTimezoneOffset();
+  if (isDaylightSaving) {
+    offset += 1;
+  }
+
+  const hours = Math.floor(Math.abs(offset));
+  const minutes = Math.floor((Math.abs(offset) * 60) % 60);
+  const sign = offset >= 0 ? '+' : '-';
+
+  return `UTC\u2009${sign}\u200A${hours.toString()}`;
+  // return `UTC${sign}${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+}
---
+++ docs/js/data.js
@@ -0,0 +1,291 @@
+export const data = {
+  experiences: [
+    {
+      start_yr: 2021,
+      start_mo: 8,
+      leave_yr: 2023,
+      leave_mo: 8,
+      lasting: "2 years",
+      company: "Baidu",
+      website: "https://ir.baidu.com/",
+      title: "Software Engineer \u00B7 Search Tech Platform R&D Dept",
+      desc: "Baidu App Backend Developer \u00B7 Golang",
+      city: "Beijing \u00B7 China",
+      current: false,
+    },
+  ],
+  educations: [
+    {
+      start_yr: 2023,
+      start_mo: 8,
+      leave_yr: 2024,
+      leave_mo: 7,
+      lasting: "Recently",
+      company: "Nanyang Technological University",
+      website: "https://www.ntu.edu.sg/index",
+      title: "Master of Science \u00B7 Artificial Intelligence",
+      desc: "School of Computer Science and Engineering",
+      city: "Singapore",
+      current: "Present",
+    },
+    {
+      start_yr: 2017,
+      start_mo: 8,
+      leave_yr: 2021,
+      leave_mo: 6,
+      lasting: "Grade: 88%",
+      company: "Harbin Institute of Technology",
+      website: "http://en.hitsz.edu.cn/",
+      title: "Bachelor of Engineering \u00B7 Communication Engineering",
+      desc: "School of Electronics and Information Engineering",
+      city: "Shenzhen \u00B7 China",
+      current: false,
+    },
+  ],
+  projects: [
+    {
+      message: "2021, Outstanding Graduation Project of Harbin Institute of Technology",
+      heading: "Melody embedding <span class=\"text-muted\">borrowed from word2vec and doc2vec</span>",
+      content: "Based on <a class=\"link-secondary text-decoration-none\" href=\"https://www.jstage.jst.go.jp/article/ipsjjip/27/0/27_278/_pdf\" target=\"_blank\">Tatsunori Hirai's melody2vec,</a> I implemented song2vec, which can map not only melody fragments but also whole songs to vectors.",
+      btn_url: "https://github.com/muhac/melody-embedding",
+      btn_txt: "Open source on GitHub",
+      img_url: "/assets/muhac_docs/img/melodic-figures.png",
+      img_alt: "Melody Segmentation",
+      img_woh: 3988 / 2441,
+    },
+    {
+      message: "2020, Chinese Computer Software Copyright Certificate",
+      heading: "Fast neural style transfer <span class=\"text-muted\">for image processing</span>",
+      content: "<a class=\"link-secondary text-decoration-none\" href=\"https://arxiv.org/abs/1603.08155\" target=\"_blank\">Johnson et al.'s fast style transfer</a> is a method for merging the content of one image with the style of another. Built and deployed as a web application.",
+      btn_url: "https://github.com/muhac/fast-neural-style-transfer",
+      btn_txt: "Open source on GitHub",
+      img_url: "/assets/muhac_docs/img/fast-neural-style-transfer.png",
+      img_alt: "Fast Neural Style Transfer",
+      img_woh: 675 / 445,
+    },
+  ],
+  skills: [
+    [{
+      "name": "Bash",
+      "logo": "/assets/muhac_docs/logo/bash.svg",
+    }, {
+      "name": "Python",
+      "logo": "/assets/muhac_docs/logo/python.svg",
+    }, {
+      "name": "Go",
+      "logo": "/assets/muhac_docs/logo/go.svg",
+    }, {
+      "name": "JavaScript",
+      "logo": "/assets/muhac_docs/logo/javascript.svg",
+    }, {
+      "name": "HTML",
+      "logo": "/assets/muhac_docs/logo/html5.svg",
+    }, {
+      "name": "CSS",
+      "logo": "/assets/muhac_docs/logo/css3.svg",
+    }, {
+      "name": "Haskell",
+      "logo": "/assets/muhac_docs/logo/haskell.svg",
+    }],
+    [{
+      "name": "Vue.js",
+      "logo": "/assets/muhac_docs/logo/vuejs.svg",
+    }, {
+      "name": "Bootstrap",
+      "logo": "/assets/muhac_docs/logo/bootstrap.svg",
+    }, {
+      "name": "Nginx",
+      "logo": "/assets/muhac_docs/logo/nginx.svg",
+    }, {
+      "name": "Django",
+      "logo": "/assets/muhac_docs/logo/django.svg",
+    }, {
+      "name": "Flask",
+      "logo": "/assets/muhac_docs/logo/flask.svg",
+    }, {
+      "name": "Gin",
+      "logo": "/assets/muhac_docs/logo/gin.svg",
+    }, {
+      "name": "PyTorch",
+      "logo": "/assets/muhac_docs/logo/pytorch.svg",
+    }],
+    [{
+      "name": "Linux",
+      "logo": "/assets/muhac_docs/logo/linux.svg",
+    }, {
+      "name": "Proxmox VE",
+      "logo": "/assets/muhac_docs/logo/proxmox.svg",
+    }, {
+      "name": "AWS",
+      "logo": "/assets/muhac_docs/logo/aws.svg",
+    }, {
+      "name": "Docker",
+      "logo": "/assets/muhac_docs/logo/docker.svg",
+    }, {
+      "name": "MySQL",
+      "logo": "/assets/muhac_docs/logo/mysql.svg",
+    }, {
+      "name": "Redis",
+      "logo": "/assets/muhac_docs/logo/redis.svg",
+    }, {
+      "name": "Git",
+      "logo": "/assets/muhac_docs/logo/git.svg",
+    }],
+  ],
+  skills_scroll_frames: [2500, 3000, 3500], //  typical 60 fps
+}
+
+const translations = {
+  "Recently": {
+    "zh-CN": "最近",
+  },
+  "Present": {
+    "zh-CN": "至今",
+  },
+
+  "Baidu": {
+    "zh-CN": "百度",
+  },
+  "Software Engineer \u00B7 Search Tech Platform R&D Dept": {
+    "zh-CN": "软件工程师 \u00B7 搜索技术平台研发部 \u00B7 MEG",
+  },
+  "Baidu App Backend Developer \u00B7 Golang": {
+    "zh-CN": "手机百度后端研发 \u00B7 主力语言 Go",
+  },
+  "Beijing \u00B7 China": {
+    "zh-CN": "中国 \u00B7 北京",
+  },
+  "2 years": {
+    "zh-CN": "2\u2009年",
+  },
+  "https://ir.baidu.com/": {
+    "zh-CN": "https://home.baidu.com/",
+  },
+
+  "Nanyang Technological University": {
+    "zh-CN": "南洋理工大学",
+  },
+  "School of Computer Science and Engineering": {
+    "zh-CN": "计算机科学与工程学院",
+  },
+  "Master of Science \u00B7 Artificial Intelligence": {
+    "zh-CN": "理学硕士 \u00B7 人工智能专业",
+  },
+  "Singapore": {
+    "zh-CN": "新加坡",
+  },
+  "https://www.ntu.edu.sg/index": {
+    "zh-CN": "https://www.ntu.edu.sg/main/cn",
+  },
+
+  "Harbin Institute of Technology": {
+    "zh-CN": "哈尔滨工业大学",
+  },
+  "School of Electronics and Information Engineering": {
+    "zh-CN": "电子与信息工程学院",
+  },
+  "Bachelor of Engineering \u00B7 Communication Engineering": {
+    "zh-CN": "工学学士 \u00B7 通信工程专业",
+  },
+  "Shenzhen \u00B7 China": {
+    "zh-CN": "中国 \u00B7 深圳",
+  },
+  "Grade: 88%": {
+    "zh-CN": "GPA: 88\u2009/\u2009100",
+  },
+  "http://en.hitsz.edu.cn/": {
+    "zh-CN": "https://www.hitsz.edu.cn/index.html",
+  },
+}
+
+export function getTranslation(lang) {
+  let translatedData = JSON.parse(JSON.stringify(data));
+
+  translatedData.experiences.forEach(translateExperiences(lang));
+  translatedData.educations.forEach(translateExperiences(lang));
+  translatedData.projects.forEach(translateProject(lang));
+
+  return translatedData;
+}
+
+function translateExperiences(lang) {
+  return (item) => {
+    item.website = translate(lang, item.website);
+    item.company = translate(lang, item.company);
+    item.title = translate(lang, item.title);
+    item.desc = translate(lang, item.desc);
+    item.city = translate(lang, item.city);
+    item.lasting = translate(lang, item.lasting);
+    if (item.current) {
+      item.current = translate(lang, item.current);
+      item.elapsed = calcElapsed(lang, item.start_yr, item.start_mo);
+    }
+  };
+}
+
+function translateProject(lang) {
+  return (item) => {
+    item.message = translate(lang, item.message);
+    item.heading = translate(lang, item.heading);
+    item.content = translate(lang, item.content);
+    item.btn_txt = translate(lang, item.btn_txt);
+    item.img_alt = translate(lang, item.img_alt);
+  };
+}
+
+function translate(lang, text) {
+  if (translations[text] && translations[text][lang]) {
+    text = translations[text][lang];
+  }
+  return text;
+}
+
+function calcElapsed(lang, year, month) {
+  const d = new Date();
+  let yr = d.getFullYear();
+  let mo = d.getMonth() + 1;
+  let da = d.getDate();
+
+  let m = 12 * (yr - year) + (mo - month);
+  if (da > 15) {
+    // m += 1;
+  }
+
+  let years = parseInt(m / 12);
+  let months = m % 12;
+
+  let english = "";
+  let chinese = "";
+
+  if (years > 0) {
+    english += years + " year";
+    chinese += years + "\u2009年";
+    if (years > 1) {
+      english += "s";
+    }
+    if (months > 0) {
+      english += " ";
+      chinese += "\u2009";
+    }
+  }
+
+  if (months > 0) {
+    english += months + " month";
+    chinese += months + "\u2009个月";
+    if (months > 1) {
+      english += "s";
+    }
+  }
+
+  if (years <= 0 && months <= 0) {
+    english = "Recently";
+    chinese = "最近";
+  }
+
+  switch (lang) {
+    case "zh-CN":
+      return chinese;
+    default:
+      return english;
+  }
+}
